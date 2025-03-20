2b6de368fe4ccfcdcc9dae37129baaab40f3fd97
Tornike Parunashvili <parunashvilitornike@gmail.com>
2025-03-20 17:35:27 +0400

    Create History and Converter Views as reusable Views

---
+++ Components/HistoryView.swift
@@ -0,0 +1,82 @@
+//
+//  HistoryView.swift
+//  CurrencyConverter
+//
+//  Created by tornike <parunashvili on 20.03.25.
+//
+
+import UIKit
+import SnapKit
+
+final class HistoryView: UIView {
+  
+  // MARK: - Properties
+  
+  private var history: [ConversionHistory] = []
+  
+  // MARK: - UI Components
+  
+  private lazy var containerView = ContainerView()
+  
+  private lazy var historyStackView: UIStackView = {
+    let stack = UIStackView()
+    stack.axis = .vertical
+    stack.spacing = 8.0
+    return stack
+  }()
+  
+  private lazy var titleLabel: UILabel = {
+    let label = UILabel()
+    label.text = "Last 5 Conversions"
+    label.font = .boldSystemFont(ofSize: 16)
+    label.textColor = .white
+    return label
+  }()
+  
+  // MARK: - Initialization
+  
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    setupUI()
+  }
+  
+  required init?(coder: NSCoder) {
+    super.init(coder: coder)
+    setupUI()
+  }
+  
+  // MARK: - Private Methods
+  
+  private func setupUI() {
+    containerView.addContent(historyStackView)
+    historyStackView.addArrangedSubview(titleLabel)
+    
+    addSubview(containerView)
+    
+    containerView.snp.makeConstraints { make in
+      make.edges.equalToSuperview()
+    }
+  }
+  
+  // MARK: - Public Methods
+  
+  func updateHistory(with history: [ConversionHistory]) {
+    self.history = history
+    
+    // Remove all history items but keep the title
+    historyStackView.arrangedSubviews.forEach { view in
+      if view != titleLabel {
+        view.removeFromSuperview()
+      }
+    }
+    
+    // Add new history items
+    history.forEach { conversion in
+      let historyLabel = UILabel()
+      historyLabel.text = "\(conversion.fromAmount) \(conversion.fromCurrency.symbol) = \(conversion.toAmount) \(conversion.toCurrency.symbol)"
+      historyLabel.font = .systemFont(ofSize: 14)
+      historyLabel.textColor = .white
+      historyStackView.addArrangedSubview(historyLabel)
+    }
+  }
+}
--- ViewController/Currency.ConverterViewController.swift
+++ ViewController/Currency.ConverterViewController.swift
@@ -18,47 +18,8 @@ final class CurrencyConverterViewController: UIViewController {
   
   // MARK: - UI Components
   
-  private lazy var selectorsContainer = ContainerView()
-  private lazy var fieldsContainer = ContainerView()
-  private lazy var historyContainer = ContainerView()
-  
-  private lazy var fromCurrencySelector: CurrencySelector = {
-    let selector = CurrencySelector(currencies: Currency.allCases, selectedCurrency: viewModel.fromCurrency)
-    return selector
-  }()
-  
-  private lazy var toCurrencySelector: CurrencySelector = {
-    let selector = CurrencySelector(currencies: Currency.allCases, selectedCurrency: viewModel.toCurrency)
-    return selector
-  }()
-  
-  private lazy var fromCurrencyField = CurrencyTextField(type: .sell)
-  private lazy var toCurrencyField = CurrencyTextField(type: .buy)
-  
-  private lazy var reverseButton = ButtonComponent(type: .reverse)
-  
-  private lazy var exchangeRateLabel: UILabel = {
-    let label = UILabel()
-    label.font = .systemFont(ofSize: 14)
-    label.textColor = .gray
-    label.textAlignment = .center
-    return label
-  }()
-  
-  private lazy var converterStackView: UIStackView = {
-    let stack = UIStackView()
-    stack.axis = .vertical
-    stack.spacing = 24.0
-    stack.alignment = .fill
-    return stack
-  }()
-  
-  private lazy var historyStackView: UIStackView = {
-    let stack = UIStackView()
-    stack.axis = .vertical
-    stack.spacing = 8.0
-    return stack
-  }()
+  private lazy var converterView = ConverterView(viewModel: viewModel)
+  private lazy var historyView = HistoryView()
   
   // MARK: - Lifecycle
   
@@ -66,10 +27,6 @@ final class CurrencyConverterViewController: UIViewController {
     super.viewDidLoad()
     setupUI()
     setupBindings()
-    setupActions()
-    
-    fromCurrencyField.text = viewModel.inputAmount
-    toCurrencyField.isUserInteractionEnabled = false
   }
   
   // MARK: - Private Methods
@@ -83,74 +40,26 @@ final class CurrencyConverterViewController: UIViewController {
   }
   
   private func addSubviews() {
-    let selectorsStackView = UIStackView(arrangedSubviews: [fromCurrencySelector, toCurrencySelector])
-    selectorsStackView.axis = .vertical
-    selectorsStackView.spacing = 8
-    selectorsContainer.addContent(selectorsStackView)
-    
-    let fieldsStackView = UIStackView(arrangedSubviews: [fromCurrencyField, toCurrencyField])
-    fieldsStackView.axis = .vertical
-    fieldsStackView.spacing = 16
-    fieldsContainer.addContent(fieldsStackView)
-    
-    fieldsContainer.contentView.addSubview(reverseButton)
-    
-    converterStackView.addArrangedSubviews([
-      selectorsContainer,
-      fieldsContainer,
-      exchangeRateLabel,
-    ])
-    
-    historyContainer.addContent(historyStackView)
-    
-    view.addSubview(converterStackView)
-    view.addSubview(historyContainer)
+    view.addSubview(converterView)
+    view.addSubview(historyView)
   }
   
   private func makeConstraints() {
-    converterStackView.snp.makeConstraints { make in
+    converterView.snp.makeConstraints { make in
       make.top.equalTo(view.safeAreaLayoutGuide).offset(20)
       make.leading.trailing.equalToSuperview().inset(20)
     }
     
-    historyContainer.snp.makeConstraints { make in
-      make.top.equalTo(converterStackView.snp.bottom).offset(20)
+    historyView.snp.makeConstraints { make in
+      make.top.equalTo(converterView.snp.bottom).offset(20)
       make.leading.trailing.equalToSuperview().inset(20)
       make.bottom.lessThanOrEqualTo(view.safeAreaLayoutGuide).inset(20)
-    }
-    
-    reverseButton.snp.makeConstraints { make in
-      make.centerX.equalToSuperview()
-      make.centerY.equalTo(fieldsContainer.contentView)
-      make.size.equalTo(50)
-    }
-  }
-  
-  private func setupActions() {
-    reverseButton.setTapHandler { [weak self] in
-      self?.viewModel.swapCurrencies()
     }
   }
   
   // MARK: - Binding Methods
   
   private func setupBindings() {
-    /// Bind the amount text field to the ViewModel's input amount
-    fromCurrencyField.textPublisher
-      .dispatchOnMainQueue()
-      .sink { [weak self] text in
-        self?.viewModel.inputAmount = text
-      }
-      .store(in: &cancellables)
-    
-    /// Bind the converted amount to the UI
-    viewModel.$convertedAmount
-      .dispatchOnMainQueue()
-      .sink { [weak self] value in
-        self?.toCurrencyField.text = value
-      }
-      .store(in: &cancellables)
-    
     /// Handle error messages and display them to the user
     viewModel.$errorMessage
       .dispatchOnMainQueue()
@@ -160,68 +69,13 @@ final class CurrencyConverterViewController: UIViewController {
       }
       .store(in: &cancellables)
     
-    /// Bind the exchange rate to the UI
-    viewModel.$exchangeRate
-      .dispatchOnMainQueue()
-      .sink { [weak self] rate in
-        self?.exchangeRateLabel.text = "Exchange Rate: \(rate)"
-      }
-      .store(in: &cancellables)
-    
-    /// Bind the currencies to the labels
-    viewModel.$fromCurrency
-      .dispatchOnMainQueue()
-      .sink { [weak self] currency in
-        self?.fromCurrencyField.updateCurrencySymbol(currency.symbol)
-      }
-      .store(in: &cancellables)
-    
-    viewModel.$toCurrency
-      .dispatchOnMainQueue()
-      .sink { [weak self] currency in
-        self?.toCurrencyField.updateCurrencySymbol(currency.symbol)
-      }
-      .store(in: &cancellables)
-    
+    /// Update history view when conversion history changes
     viewModel.$conversionHistory
       .dispatchOnMainQueue()
       .sink { [weak self] history in
-        self?.updateHistoryView(with: history)
+        self?.historyView.updateHistory(with: history)
       }
       .store(in: &cancellables)
-    
-    fromCurrencySelector.currencyPublisher
-      .sink { [weak self] currency in
-        self?.viewModel.fromCurrency = currency
-      }
-      .store(in: &cancellables)
-    
-    toCurrencySelector.currencyPublisher
-      .sink { [weak self] currency in
-        self?.viewModel.toCurrency = currency
-      }
-      .store(in: &cancellables)
-    
-  }
-  
-  // MARK: - Update History View
-  
-  private func updateHistoryView(with history: [ConversionHistory]) {
-    historyStackView.arrangedSubviews.forEach { $0.removeFromSuperview() }
-    
-    let titleLabel = UILabel()
-    titleLabel.text = "Last 5 Conversions"
-    titleLabel.font = .boldSystemFont(ofSize: 16)
-    titleLabel.textColor = .white
-    historyStackView.addArrangedSubview(titleLabel)
-    
-    history.forEach { conversion in
-      let historyLabel = UILabel()
-      historyLabel.text = "\(conversion.fromAmount) \(conversion.fromCurrency.symbol) = \(conversion.toAmount) \(conversion.toCurrency.symbol)"
-      historyLabel.font = .systemFont(ofSize: 14)
-      historyLabel.textColor = .white
-      historyStackView.addArrangedSubview(historyLabel)
-    }
   }
   
   // MARK: - Error Handling
